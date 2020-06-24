6dc8c699eac254cd5f1cdd6f6e8e5cd2eac03925
TTricker2 <jhu5689@naver.com>
2020-06-24 21:25:58 +0900

    Update 11726T.c

--- DP/11726T.c
+++ DP/11726T.c
@@ -4,7 +4,7 @@ int topDown(int n)
 {
 	if (n==0 || n==1) return 1;
 	if (memo[n]>0)
-		return memo[n];
+		return memo[n]; //위의 if조건을 쓰지 않으면 시간초과가 나온다.
 	memo[n] = topDown(n-1) + topDown(n-2);
 	memo[n] %= 10007;
 	return memo[n];
